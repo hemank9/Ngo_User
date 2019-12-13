26125797ed1b7d444d6d830054d25a892754044f
Hemank Tank <hemank9697@gmail.com>
2019-12-14 02:05:54 +0530

    Home page ngo design

---
+++ app/src/main/java/com/example/newprongo/ngonamelist.java
@@ -0,0 +1,14 @@
+package com.example.newprongo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ngonamelist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngonamelist);
+    }
+}
