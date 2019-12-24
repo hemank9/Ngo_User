9430033b4f41896d72abfb83aa72d5075bf32468
Hemank Tank <hemank9697@gmail.com>
2019-12-24 21:38:27 +0530

    User home page design

---
+++ app/src/main/java/com/example/newprongo/Profile_ngo.java
@@ -0,0 +1,14 @@
+package com.example.newprongo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_ngo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_ngo);
+    }
+}
